1415bf72def480c799fac2cbf5a78ddd1265833f
nathanlct <nathanlct@icloud.com>
2019-07-05 17:37:59 -0700

    remove unnecessary double

--- src/SoundInterface.cpp
+++ src/SoundInterface.cpp
@@ -2,7 +2,7 @@
 
 
 float SoundInterface::get_sample(float elapsed) {
-    float amplitude = 0.5f;
+    float amplitude = 0.3f;
     float frequency = 400.0f;
-    return amplitude * sin(((double)frequency) * ((double)2 * 3.14159) * ((double)elapsed));
+    return amplitude * sin(2 * M_PI * frequency * elapsed);
 }
